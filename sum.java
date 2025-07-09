3c10cf59550fd2776e55fdb624d3b8df1c561eb8
yash-kacha <yashkacha2705@gmail.com>
2025-07-09 11:14:56 +0530

    trying

--- sum.java
+++ sum.java
@@ -4,5 +4,32 @@ public class sum {
         int sum = a + b;
         System.out.println("Sum: " + sum);
     }
+
+    public static int doSum(int a ,int b)
+    {
+        return a+b;
+    }
+
+    public static void checkTest() {
+        testDoSum(5, 3, 8);
+        testDoSum(0, 0, 0);
+        testDoSum(-2, 2, 0);
+        testDoSum(100, 200, 300);
+        testDoSum(-5, -3, -8);
+        testDoSum(Integer.MAX_VALUE, 0, Integer.MAX_VALUE);
+        testDoSum(Integer.MIN_VALUE, 0, Integer.MIN_VALUE);
+        testDoSum(12345, 67890, 80235);
+        testDoSum(-100, 50, -50);
+        testDoSum(9999, -9999, 0);
+    }
+    
+    private static void testDoSum(int a, int b, int expected) {
+        int result = doSum(a, b);
+        if (result == expected) {
+            System.out.println("Test passed: doSum(" + a + ", " + b + ") == " + expected);
+        } else {
+            System.out.println("Test failed: doSum(" + a + ", " + b + ") returned " + result + ", expected " + expected);
+        }
+    }
 //yash is here
 }
